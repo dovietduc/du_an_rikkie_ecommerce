821300f59880dc78201f48155207d6484ccb84d3
ducdo <tvietduc122@gmail.com>
2023-07-14 11:31:05 +0700

    valiadate sucsss

--- assets/js/pages/signup.js
+++ assets/js/pages/signup.js
@@ -6,6 +6,7 @@ let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 let passwordSelector = document.querySelector('.password');
 let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
 let confirmPasswordSelector = document.querySelector('.confirm_password');
+let tooglePass = document.querySelector('.toogle_password');
 
 
 function showError(input, message) {
@@ -15,6 +16,9 @@ function showError(input, message) {
     let messageInput = input.nextElementSibling;
     // Hiển thị message lỗi
     messageInput.innerText = message;
+
+    // thêm class error cho parent
+    input.closest('.form-group').classList.add('form-group_error');
 }
 
 function showSuccess(input) {
@@ -24,23 +28,52 @@ function showSuccess(input) {
     let messageInput = input.nextElementSibling;
     // Xóa message lỗi
     messageInput.innerText = '';
+
+    // xóa class error cho parent
+    input.closest('.form-group').classList.remove('form-group_error');
 }
 
 // 2. Nơi khai báo các hàm lắng nghe sự kiện hoặc hàm chạy lần đầu load trang
 function handleSignUp(event) {
     event.preventDefault();
-    let valueName = nameSelector.value.trim();
-    let valueEmail = emailSelector.value.trim();
-    let passwordValue = passwordSelector.value.trim();
-    let confirmPassValue = confirmPasswordSelector.value.trim();
+    // Validate name
+    validateName();
+    // Validate email
+    validateEmail();
+    // validate password
+    validatePassword();
+    // Validate confirm password
+    validateConfirmPassword();
+}
 
+// hàm này xử lí show hide password
+function handleTooglePasss(event) {
+    let typePass = passwordSelector.getAttribute('type');
+    let clicked = event.target;
+    if(typePass === 'password') {
+        passwordSelector.setAttribute('type', 'text');
+        clicked.classList.remove('fa-eye-slash');
+        clicked.classList.add('fa-eye');
+    } else {
+        passwordSelector.setAttribute('type', 'password');
+        clicked.classList.add('fa-eye-slash');
+        clicked.classList.remove('fa-eye');
+    }
+   
+}
+
+function validateName() {
+    let valueName = nameSelector.value.trim();
     // Validate name
     if(valueName === '') {
         showError(nameSelector, 'Tên không được để trống');
     } else {
         showSuccess(nameSelector);
     }
+}
 
+function validateEmail() {
+    let valueEmail = emailSelector.value.trim();
     // Validate email
     if(valueEmail === '') {
         showError(emailSelector, 'Email không được để trống');
@@ -49,7 +82,10 @@ function handleSignUp(event) {
     } else {
         showSuccess(emailSelector);
     }
+}
 
+function validatePassword() {
+    let passwordValue = passwordSelector.value.trim();
     // validate password
     if(passwordValue === '') {
         showError(passwordSelector, 'Password không được để trống');
@@ -60,7 +96,11 @@ function handleSignUp(event) {
     } else {
         showSuccess(passwordSelector);
     }
+}
 
+function validateConfirmPassword() {
+    let confirmPassValue = confirmPasswordSelector.value.trim();
+    let passwordValue = passwordSelector.value.trim();
     // Validate confirm password
     if(confirmPassValue === '') {
         showError(confirmPasswordSelector, 'Confirm password không được trống');
@@ -73,9 +113,17 @@ function handleSignUp(event) {
     } else {
         showSuccess(confirmPasswordSelector);
     }
-    
 }
 
 
 // 3. Nơi chạy hàm hoặc thêm addEventLisner
+// Khi nhấn vào button register
 buttonRegister.addEventListener('click', handleSignUp);
+// Khi nhấn vào icon show hide password
+tooglePass.addEventListener('click', handleTooglePasss);
+
+// Bắt sự kiện keyup cho ô input name
+nameSelector.addEventListener('keyup', validateName);
+emailSelector.addEventListener('keyup', validateEmail);
+passwordSelector.addEventListener('keyup', validatePassword);
+confirmPasswordSelector.addEventListener('keyup', validateConfirmPassword)
